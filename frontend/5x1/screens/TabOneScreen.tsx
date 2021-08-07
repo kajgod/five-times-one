888ae7383397d37c2b6c3a5b64ff0a7c777b7980
kajgod <kajgod@gmail.com>
2021-08-07 21:53:27 +0200

    ignore: testing github credentials

--- frontend/5x1/screens/TabOneScreen.tsx
+++ frontend/5x1/screens/TabOneScreen.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One test</Text>
+      <Text style={styles.title}>Tab One test 2</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
